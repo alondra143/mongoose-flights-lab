f9b451fc8ede95742223c65b97f1602215353d89
alondra 3 <not given>
2021-09-23 18:43:04 -0700

    working post request

--- controllers/flights.js
+++ controllers/flights.js
@@ -3,12 +3,26 @@ const Flight = require('../models/flight');
 module.exports = {
     index,
     new: newFlight,
+    create,
 };
 
+function create(req, res) {
+    // change departs from a string to a date value
+    req.body.departs = new Date(req.body.departs);
+    // generate random flight number for each request
+    req.body.flightNo = Math.floor(Math.random() * 9999);
+    // use Model to talk to database and create new Movie
+    Flight.create(req.body, function(err, createdFlight){
+        if (err){
+            res.redirect('/flights/new');
+        }
+        console.log(createdFlight, '<-- new Flight Added');
+        res.redirect('/flights');
+    })
+}
 function newFlight(req, res) {
     res.render('flights/new');
-}
-
+};
 
 function index(req, res) {
     Flight.find({}, function(err, flightsObjects) {
